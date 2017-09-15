196f04f2e59e5705504f5aa1723f7c2fdb0478b7
synion <lach.szymon86@gmail.com>
2017-09-15 17:53:11 +0200

    Update ImportMatchesSpec

--- spec/services/import_matches_spec.rb
+++ spec/services/import_matches_spec.rb
@@ -78,7 +78,7 @@ describe ImportMatches do
         ImportMatches.call
       end.to change{ FootballMatches.count}.by(4).and change{ Round.count }.by(2)
 
-       # match_array
+       # match_arrayh
       expect(Round.all).to include(
         have_attributes(year: '2017', number: 1),
         have_attributes(year: '2017', number: 2),
@@ -93,9 +93,84 @@ describe ImportMatches do
         have_attributes(match_data: "2017/11/20 12:30", home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
         have_attributes(match_data: "2017/11/21 12:45", home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
       )
-     end
-   end
- end
+    end
+  end
+  context 'when one round was completed after call first' do
+    it 'will update score' do
+      matches_table_page_1 = <<~RATE_PLAN
+        WEEK 1
+        Time              | Team  home  | Team away     | Score away | Score home | completed
+        ------------------+-------------+---------------+------------+------------------------
+        2017/09/10 12:30  | Monaco      |   Porto       |     0      |      0     | false
+        2017/09/11 12:45  | Real Madryt |   Barcelona   |     0      |      0     | false
+       RATE_PLAN
+      matches_table_page_2 = <<~RATE_PLAN
+      WEEK 2
+        Time              | Team home   | Team away     | Score away | Score home | completed
+        ------------------+-------------+---------------+-------------------------------------
+        2017/11/20 12:30  | Monaco      |   Real Madryt |      0     |      0     | false
+        2017/11/21 12:45  | Barcelona   |   Porto       |      0     |      0     | false
+      RATE_PLAN
+
+      ImportMatches.call
+
+      allow(GoalComWrapper).to receive(:call).with(1) { result_from(matches_table_page_1) }
+      allow(GoalComWrapper).to receive(:call).with(2) { result_from(matches_table_page_2) }
+
+      expect do
+        ImportMatches.call
+      end.to change{ FootballMatch.count}.by(4).and change{ Round.count }.by(2)
+
+       # match_array
+      expect(Round.all).to include(
+        have_attributes(year: '2017', number: 1),
+        have_attributes(year: '2017', number: 2),
+      )
+
+      round_first = Round.find_by(year: '2017', number: 1)
+      round_second = Round.find_by(year: '2017', number: 2)
+
+      expect(FootballMatch.all).to include(
+        have_attributes(match_data: DateTime.parse("2017/09/10 12:30"), home_team_id: monaco.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id:round_first.id ),
+        have_attributes(match_data: DateTime.parse("2017/09/11 12:45"), home_team_id: real.id, away_team_id: barcelona.id, home_team_score: nil, away_team_score: nil, round_id: round_first.id),
+        have_attributes(match_data: DateTime.parse("2017/11/20 12:30"), home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
+        have_attributes(match_data: DateTime.parse("2017/11/21 12:45"), home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
+      )
+
+      matches_table_page_1_after_call = <<~RATE_PLAN
+        WEEK 1
+        Time              | Team  home  | Team away     | Score away | Score home | completed
+        ------------------+-------------+---------------+------------+------------------------
+        2017/09/10 12:30  | Monaco      |   Porto       |     1      |      5     | true
+        2017/09/11 12:45  | Real Madryt |   Barcelona   |     0      |      0     | false
+       RATE_PLAN
+      matches_table_page_2_after_call = <<~RATE_PLAN
+      WEEK 2
+        Time              | Team home   | Team away     | Score away | Score home | completed
+        ------------------+-------------+---------------+-------------------------------------
+        2017/11/20 12:30  | Monaco      |   Real Madryt |      0     |      0     | false
+        2017/11/21 12:45  | Barcelona   |   Porto       |      0     |      0     | false
+      RATE_PLAN
+
+      allow(GoalComWrapper).to receive(:call).with(1) { result_from(matches_table_page_1_after_call) }
+      allow(GoalComWrapper).to receive(:call).with(2) { result_from(matches_table_page_2_after_call) }
+
+      expect do
+        ImportMatches.call
+      end.to not_change{ FootballMatch.count}.and not_change{ Round.count }
+
+      round_first = Round.find_by(year: '2017', number: 1)
+      round_second = Round.find_by(year: '2017', number: 2)
+
+      expect(FootballMatch.all).to include(
+        have_attributes(match_data: DateTime.parse("2017/09/10 12:30"), home_team_id: monaco.id, away_team_id: porto.id, home_team_score: 1, away_team_score: 5, round_id:round_first.id ),
+        have_attributes(match_data: DateTime.parse("2017/09/11 12:45"), home_team_id: real.id, away_team_id: barcelona.id, home_team_score: nil, away_team_score: nil, round_id: round_first.id),
+        have_attributes(match_data: DateTime.parse("2017/11/20 12:30"), home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
+        have_attributes(match_data: DateTime.parse("2017/11/21 12:45"), home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
+      )
+    end
+  end
+end
 
  # allow(GoalComWrapper).to receive(:get_round).with(1) { result_from(matches_table_page_1) }
  # allow(GoalComWrapper).to receive(:get_round).with(2) { result_from(matches_table_page_2) }
