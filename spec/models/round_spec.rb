55b7938f7881301225f9701e270452358ada7962
Synion <lach.szymon86@gmail.com>
2017-09-18 13:42:40 +0200

    Remove unimportant spec

--- spec/models/round_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Round, type: :model do
-  describe 'database columns' do
-    it { should have_db_column :name }
-    it { should have_db_column :start_date }
-    it { should have_db_column :end_date }
-  end
-end
