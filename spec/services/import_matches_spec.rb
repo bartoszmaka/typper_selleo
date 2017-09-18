3b9b7376c66213abbfb851cb0f64c710604b2fd5
Synion <lach.szymon86@gmail.com>
2017-09-18 13:46:53 +0200

    Improve spec and result method ImportMatches

--- spec/services/import_matches_spec.rb
+++ spec/services/import_matches_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe ImportMatches do
-
   context 'when there are two rounds with two matches each' do
     context 'when there are no completed matches' do
       it 'imports all matches and rounds properly and score set to nil' do
@@ -43,14 +42,15 @@ describe ImportMatches do
         round_second = Round.find_by(year: 2017, number: 2)
 
         expect(FootballMatch.all).to include(
-          have_attributes(match_date: DateTime.parse("2017/09/10 12:30"), home_team_id: monaco.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id:round_first.id ),
-          have_attributes(match_date: DateTime.parse("2017/09/11 12:45"), home_team_id: real.id, away_team_id: barcelona.id, home_team_score: nil, away_team_score: nil, round_id: round_first.id),
-          have_attributes(match_date: DateTime.parse("2017/11/20 12:30"), home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
-          have_attributes(match_date: DateTime.parse("2017/11/21 12:45"), home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id)
+          have_attributes(match_date:  DateTime.new(2017, 9, 10, 12, 30), home_team_id: monaco.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id:round_first.id ),
+          have_attributes(match_date:  DateTime.new(2017, 9, 11, 12, 45), home_team_id: real.id, away_team_id: barcelona.id, home_team_score: nil, away_team_score: nil, round_id: round_first.id),
+          have_attributes(match_date:  DateTime.new(2017, 11, 20, 12, 30), home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
+          have_attributes(match_date:  DateTime.new(2017, 11, 21, 12, 45), home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id)
         )
       end
     end
   end
+
   context 'when one round is completed' do
     it 'two Matches will have set score' do
       matches_table_page_1 = <<~RATE_PLAN
@@ -91,13 +91,14 @@ describe ImportMatches do
       round_second = Round.find_by(year: 2017, number: 2)
 
       expect(FootballMatch.all).to include(
-        have_attributes(match_date: DateTime.parse("2017/09/10 12:30"), home_team_id: monaco.id, away_team_id: porto.id, home_team_score: 2, away_team_score: 1, round_id:round_first.id),
-        have_attributes(match_date: DateTime.parse("2017/09/11 12:45"), home_team_id: real.id, away_team_id: barcelona.id, home_team_score: 3, away_team_score: 5, round_id: round_first.id),
-        have_attributes(match_date: DateTime.parse("2017/11/20 12:30"), home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
-        have_attributes(match_date: DateTime.parse("2017/11/21 12:45"), home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id)
+        have_attributes(match_date:  DateTime.new(2017, 9, 10, 12, 30), home_team_id: monaco.id, away_team_id: porto.id, home_team_score: 2, away_team_score: 1, round_id:round_first.id),
+        have_attributes(match_date:  DateTime.new(2017, 9, 11, 12, 45), home_team_id: real.id, away_team_id: barcelona.id, home_team_score: 3, away_team_score: 5, round_id: round_first.id),
+        have_attributes(match_date:  DateTime.new(2017, 11, 20, 12, 30), home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
+        have_attributes(match_date:  DateTime.new(2017, 11, 21, 12, 45), home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id)
       )
     end
   end
+
   context 'when one round was completed after call first' do
     it 'will update score' do
       matches_table_page_1 = <<~RATE_PLAN
@@ -139,10 +140,10 @@ describe ImportMatches do
       round_second = Round.find_by(year: 2017, number: 2)
 
       expect(FootballMatch.all).to include(
-        have_attributes(match_date: DateTime.parse("2017/09/10 12:30"), home_team_id: monaco.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id:round_first.id ),
-        have_attributes(match_date: DateTime.parse("2017/09/11 12:45"), home_team_id: real.id, away_team_id: barcelona.id, home_team_score: nil, away_team_score: nil, round_id: round_first.id),
-        have_attributes(match_date: DateTime.parse("2017/11/20 12:30"), home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
-        have_attributes(match_date: DateTime.parse("2017/11/21 12:45"), home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
+        have_attributes(match_date: DateTime.new(2017, 9, 10, 12, 30), home_team_id: monaco.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id:round_first.id ),
+        have_attributes(match_date: DateTime.new(2017, 9, 11, 12, 45), home_team_id: real.id, away_team_id: barcelona.id, home_team_score: nil, away_team_score: nil, round_id: round_first.id),
+        have_attributes(match_date: DateTime.new(2017, 11, 10, 12, 30), home_team_id: monaco.id, away_team_id: real.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
+        have_attributes(match_date: DateTime.new(2017, 11, 21, 12, 45), home_team_id: barcelona.id, away_team_id: porto.id, home_team_score: nil, away_team_score: nil, round_id: round_second.id),
       )
 
       matches_table_page_1_after_call = <<~RATE_PLAN
@@ -184,16 +185,18 @@ end
   def result_from(tabular_data)
     round_number = tabular_data.lines.first.split(' ').second.to_i
     tabular_data.lines[3..-1].map do |line|
-      match_attribute_values = line.split('|').map(&:strip)
-        match_date = DateTime.parse(match_attribute_values[0])
+      date, home_team_name, away_team_name, home_team_score,
+        away_team_score, completed = line.split('|').map(&:strip)
+        match_date = DateTime.parse(date)
       {
         match_date: match_date,
-        home_team_name: match_attribute_values[1],
-        away_team_name: match_attribute_values[2],
-        home_team_score: match_attribute_values[3].to_i,
-        away_team_score: match_attribute_values[4].to_i,
-        completed: match_attribute_values[5] == 'true',
-        round_id: Round.find_by(year: match_date.year, number: round_number )
+        home_team_name: home_team_name,
+        away_team_name: away_team_name,
+        home_team_score: home_team_score.to_i,
+        away_team_score: away_team_score.to_i,
+        completed: completed == 'true',
+        round_year: match_date.year,
+        round_number: round_number
       }
     end
   end
