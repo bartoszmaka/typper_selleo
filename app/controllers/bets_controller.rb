54ac3ac399ec8ba63af84565581e6e2eb5202e64
Synion <lach.szymon86@gmail.com>
2017-10-06 12:40:03 +0200

    Fix flash messages and bet form

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,42 +1,41 @@
 class BetsController < ApplicationController
   def edit
-    render locals: { bet: bet,
-                     football_match: FootballMatch.find(params[:football_match_id])}
+    render locals: { form: BetForm.new(bet, {}),
+                     football_match: football_match }
   end
 
   def new
-    render locals: { bet: Bet.new,
-                     football_match: FootballMatch.find(params[:football_match_id])}
+    render locals: { form: BetForm.new(Bet.new, {}),
+                     football_match: football_match }
   end
 
   def update
-    bet = Bet.find(params[:id])
     form = BetForm.new(bet,
                        params[:bet].merge(user_id: current_user.id,
                                           football_match_id: params[:football_match_id]))
     if form.save
       redirect_to root_path, notice: 'Bet succesfully updated'
     else
-      flash[:alert] = 'Could not update bet'
-      render 'edit'
+      flash[:error] = 'Could not update bet'
+      render 'edit', locals: { form: form,
+                               football_match: football_match }
     end
   end
 
   def create
-    bet = Bet.new
-    form = BetForm.new(bet,
+    form = BetForm.new(Bet.new,
                        params[:bet].merge(user_id: current_user.id,
                                           football_match_id: params[:football_match_id]))
     if form.save
       redirect_to root_path, notice: 'Bet succesfully created'
     else
-      flash[:alert] = 'Could not create bet'
-      render 'new'
+      flash[:error] = 'Could not create bet'
+      render 'new', locals: { form: form,
+                              football_match: football_match }
     end
   end
 
   def destroy
-    bet = Bet.find(params[:id])
     if bet.destroy
       redirect_to root_path, notice: 'Bet succesfully deleted'
     else
@@ -48,5 +47,9 @@ class BetsController < ApplicationController
 
   def bet
     @bet ||= Bet.find(params[:id])
+  end
+
+  def football_match
+    @football_match ||= FootballMatch.find(params[:football_match_id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   def bootstrap_class_for(flash_type)
     case flash_type.to_sym
     when :success then 'alert-success'
-    when :error then 'alert-error'
-    when :alert then 'alert-block'
+    when :error then 'alert-danger'
+    when :alert then 'alert-warning'
     when :notice then 'alert-info'
     else flash_type.to_s
     end
